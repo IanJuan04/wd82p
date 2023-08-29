87f228ccc6b0acbfd1130a860e5b4d55ebd03205
Adrian Juan <adrian_juan99@yahoo.com>
2023-08-29 21:14:39 +0800

    Finished my very first JS Exercise

---
+++ 15-javascript_fundamentals/main.js
@@ -0,0 +1,28 @@
+//Exercise no.1
+let firstNumber = 23 + 97;
+console.log(firstNumber);
+
+//Exercise no.2
+let secondNumber = 2 + 4 + 6 + 8 + 10 + 12;
+console.log(secondNumber);
+
+//Exercise no.3
+let thirdNumber = (4 + 6 + 9) / 77;
+console.log(thirdNumber);
+
+//Exercise no.4
+let a = 10;
+console.log(a);
+console.log(9 * a);
+
+let b = 7 * a;
+console.log(b);
+
+//Exercise no.5
+const MAX = 57;
+
+let actual = MAX - 13;
+
+let percentage = actual / MAX;
+
+console.log(percentage);
