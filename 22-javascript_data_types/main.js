2a0384dbc47a7ae746dbba7ddad9fa000208b369
Adrian Juan <adrian_juan99@yahoo.com>
2023-09-05 20:56:08 +0800

    Added new learning about Javascript Data types and Loops

---
+++ 22-javascript_data_types/main.js
@@ -0,0 +1,25 @@
+let fruits = ["Apple", "Banana", "Orange", "Grape", "Strawberry"];
+
+const vegetables = ["Tomato", "Potato", "Onion", "Cucumber"];
+const meats = ["Chicken", "Pork", "Beef", "Lamb"];
+
+const food = fruits.concat(vegetables, meats);
+
+console.log(food);
+
+//Loops
+const fruits = ["Apple", "Banana", "Orange"];
+
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
+
+// for (let fruit of fruits) {
+//   console.log(fruit);
+// }
+
+const cars = ["Honda", "Toyota", "BMW"];
+
+for (let car = 0; car < cars.length; car++) {
+  console.log(cars[car]);
+}
