57e13f79d59f4aee7fbe7463b303a090c91dc486
Adrian Juan <adrian_juan99@yahoo.com>
2023-09-12 20:51:19 +0800

    Updated the Exercises

--- Asynch_basic_exercise/main.js
+++ Asynch_basic_exercise/main.js
@@ -8,54 +8,54 @@ console.log(addNumbers(4, 23));
 
 // Problem 2
 
-// function factorial(n) {
-//   if (n === 0 || n === 1) return 1;
+function factorial(n) {
+  if (n === 0 || n === 1) return 1;
 
-//   let result = 1;
+  let result = 1;
 
-//   for (let i = 1; 1 <= n; i++) result * i;
-//   return result;
-// }
-// console.log(factorial(8));
+  for (let i = 1; 1 <= n; i++) result * i;
+  return result;
+}
+console.log(factorial(8));
 
-// // Problem 3
+// Problem 3
 
-// function reverseString(str) {
-//   let reversedStr = "";
+function reverseString(str) {
+  let reversedStr = "";
 
-//   for (let i = str.length - 1; i >= 0; i--) {
-//     reverseString += str.charAt(i);
-//   }
-//   return reversedStr;
-// }
-// console.log(reverseString("Hello"));
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverseString += str.charAt(i);
+  }
+  return reversedStr;
+}
+console.log(reverseString("Hello"));
 
-// // Problem 4
+// Problem 4
 
-// function findLargestNumber(arr) {
-//   let largest = arr[0];
+function findLargestNumber(arr) {
+  let largest = arr[0];
 
-//   for (let i = 1; i < arr.length; i++) {
-//     if (arr[1] > largest) {
-//       largest = arr[i];
-//     }
-//   }
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[1] > largest) {
+      largest = arr[i];
+    }
+  }
 
-//   return largest;
-// }
-// console.log(findLargestNumber([5, 1, 3, 22]));
-// // Another way to find the largest number
-// function findTheLargestNumber(arr) {
-//   return Math.max(...arr);
-// }
+  return largest;
+}
+console.log(findLargestNumber([5, 1, 3, 22]));
+// Another way to find the largest number
+function findTheLargestNumber(arr) {
+  return Math.max(...arr);
+}
 
-// console.log(findTheLargestNumber([5, 1, 3, 22]));
+console.log(findTheLargestNumber([5, 1, 3, 22]));
 
-// // Problem 5
-// // connected to Problem 3
-// function isPalindrome(str) {
-//   return str === reverseString(str);
-// }
+// Problem 5
+// connected to Problem 3
+function isPalindrome(str) {
+  return str === reverseString(str);
+}
 
-// console.log(isPalindrome("racecar"));
-// console.log(isPalindrome("hello"));
+console.log(isPalindrome("racecar"));
+console.log(isPalindrome("hello"));
